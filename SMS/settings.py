4de2523df5374092ab09beea511d5123329a43b0
ak2502 <akanksha.feb25@.com>
2021-07-16 10:14:14 +0530

    login logout added

--- SMS/settings.py
+++ SMS/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL="sms_app.CustomUser"
+AUTHENTICATION_BACKENDS=['sms_app.EmailBackEnd.EmailBackEnd']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- sms_app/views.py
+++ sms_app/views.py
@@ -1,6 +1,39 @@
+import datetime
+
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from sms_app.EmailBackEnd import EmailBackEnd
 
 # Create your views here.
 
 def showDemoPage(request):
     return render(request,"demo.html")
+
+def ShowLoginPage(request):
+    return render(request,"login_page.html")
+
+def doLogin(request):
+    if request.method!="POST":
+        return HttpResponse("<h2>Method Not Allowed</h2>")
+    else:
+        user=EmailBackEnd.authenticate(request,username=request.POST.get("email"),password=request.POST.get("password"))
+        if user!=None:
+            login(request,user)
+            return HttpResponse("Email : "+request.POST.get("email")+" Password : "+request.POST.get("password"))
+        else:
+            messages.error(request,"Invalid Login Details")
+            return HttpResponseRedirect("/")
+
+def GetUserDetails(request):
+    if request.user!=None:
+        return HttpResponse("User : "+request.user.email+" usertype : "+str(request.user.user_type))
+    else:
+        return HttpResponse("Please Login First")
+
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect("/")
